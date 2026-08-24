aae667fdfd31084781026f5fb343b896a211c9e0
Suhail <suhailjones4@gmail.com>
2026-08-24 00:41:36 -0400

    Chunking done

--- build_index.py
+++ build_index.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 import os
 
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import requests
 load_dotenv()
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.documents import Document
+from bs4 import BeautifulSoup
 
 
 headers = {
@@ -21,15 +23,39 @@ if os.path.isdir(INDEX_DIR) and os.listdir(INDEX_DIR):
     vectorstore = None
 else:
     print(f"No index found at '{INDEX_DIR}/' — building from scratch.")
-    # TODO: load sources.txt + the 3 extra pages, chunk, embed, build the FAISS index
+    # TODO: load sources.txt + the 5 extra pages, chunk, embed, build the FAISS index
     
+    initialSources = [
+        "https://bulletin.uga.edu/Program/Details/73962",
+        "https://bulletin.uga.edu/UnivInfo/content/university-info-undergrad-graduation-requirements.html",
+        "https://cs.uga.edu/bachelor-science-computer-science",
+        "https://doubledawgs.uga.edu/ProgramDetails/11937",
+        "https://doubledawgs.uga.edu/ProgramDetails/10080"
+    ]
+
+    #Function to remove uneccesary elements from sources' html
+    def extract_text(html):
+        soup = BeautifulSoup(html, 'html.parser')
+        for tag in soup.find_all(['nav', 'header', 'footer', 'script', 'style']):
+            tag.decompose()
+        return soup.get_text(separator=' ', strip=True)
+
     docs: list[Document] = []
     with open('sources.txt', 'r') as file:
         for line in file:
             line = line.strip()
             response = requests.get(line, timeout=20, headers=headers)
-            docs.append(Document(page_content=response.text, metadata={"source": line}))
+            docs.append(Document(page_content=extract_text(response.text), metadata={"source": line}))
+
+    for src in initialSources:
+        response = requests.get(src, timeout=20, headers=headers)
+        docs.append(Document(page_content=extract_text(response.text), metadata={"source": src}))
     print(f"Loaded {len(docs)} docs.")
+    
+    #Splits the doc list into smaller chunks of 1000 characters
+    textSplitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+    all_splits = textSplitter.split_documents(docs)
+    print(f'Split sources into {len(all_splits)} chunks.')
 
     vectorstore = None
     # TODO: save it with vectorstore.save_local(INDEX_DIR)
