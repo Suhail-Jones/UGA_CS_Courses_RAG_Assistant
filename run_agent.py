f2d02b81d6532f9e82dcb00a74e4d99b87a994ac
Suhail <suhailjones4@gmail.com>
2026-08-25 19:10:30 -0400

    Started run_agent.py

---
+++ run_agent.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import os
+
+import uuid
+from deepagents.backends import StateBackend
+from langchain.tools import tool
+
+
+backend = StateBackend
+
+@tool(parse_docstring = True)
+def search_documentation(query):
+"""
+
+
+"""
