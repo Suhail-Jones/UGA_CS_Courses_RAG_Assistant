94b4587950984f396eb95cc79ecf045bbeb0f884
Suhail <suhailjones4@gmail.com>
2026-08-26 17:25:01 -0400

    Updated requirements.txt again

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 
 import uuid
+from deepagents import create_deep_agent
 from deepagents.backends import StateBackend
 from langchain.tools import tool
 
@@ -10,3 +11,6 @@ backend = StateBackend
 
 @tool(parse_docstring = True)
 def search_documentation(query):
+    """
+    
+    """
